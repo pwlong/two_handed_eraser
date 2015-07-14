bb4e6b68e154d2e4ee99c068af45baa94a50e9b0
pwlong <paul@thelongs.ws>
2015-07-14 12:30:21 -0700

    moved structs, #defines and forward def's into header file primitive statistics working (probably not correctly) -t and -d flags getting set

---
+++ cache_controller_sim/cache_sim.h
@@ -0,0 +1,53 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+#define SETS  1024
+#define LINES 4
+#define SET_MASK  0x00007FE0
+#define SET_SHIFT 5
+#define TAG_MASK  0xFFFF8000
+#define TAG_SHIFT 15
+
+#define MATCH 0
+#define TRUE  1
+#define FALSE 0
+
+typedef unsigned char		u8;
+typedef unsigned short		u16;
+typedef unsigned int		u32;
+#ifdef __HAS_LONG_LONG__
+typedef unsigned long long 	u64;
+#endif
+
+
+// this is a cache line. It includes a pointer to the history of that line
+typedef struct {
+	int 		valid_bit;
+	int 		dirty_bit;
+	int 		LRU_bits;
+	int			tag;	
+	struct hist_node	*head;
+} cache_line;
+
+typedef struct {
+	cache_line	c_line;
+	int			set;
+	int			line;
+	struct hist_node	*next;
+} hist_node;
+
+typedef struct {
+	char command;
+	u32	 address;
+} mem_access;
+
+//function prototype
+void init_cache();
+void cache_search(int, char*);
+void add_cache_line(int, int, int, char*);
+void replace_cache_line(int, int, char*);
+int  log_command(FILE *fh, char *cmd, int addr); // take file handle, command and address
+void show_stats();
+
+
+#endif
