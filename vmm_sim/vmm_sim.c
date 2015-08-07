40f9ae4f1ce3e0a5f2647b8ace84f3026353b253
jqnguyen <jqn@pdx.edu>
2015-08-07 10:42:35 -0700

    add the static calculation

--- vmm_sim/vmm_sim.c
+++ vmm_sim/vmm_sim.c
@@ -71,22 +71,25 @@ u8	 t_flag;								// various debug flags
 u8	 d_flag;
 u8	 v_flag;
 
-
-/*		Global statistics trackers					*/
-u32	num_accesses;							
-u32 num_writes;
-u32	num_cycles;
-u32 num_cycles_no_vmm;
-u32 num_swap_ins;
-u32 num_swap_outs;
-u32 num_pure_replace;
-u32 used_pf;                         		// num_PD_entries_used;
-u32 num_PT_total;
-u32 num_PT_max;
-u32 num_UP_total;
-u32 num_UP_max;
-
 PF_t * PTBR;
+u32  used_pf   = 0;                         // tracking numbers of pf used
+
+int total_cycles   = 0;
+
+int num_PT     = 0;                          //number of PT and max number of PT
+int max_num_PT = 0;                          //was used
+
+int num_UP     = 0;                          //number of UP and max number of UP 
+int max_num_UP = 0;                          //was used
+
+int num_physical_frames = 0;
+
+int num_swap_in = 0;                         //number of swap-in and out ops
+int num_swap_out= 0;
+
+int num_pure_replace = 0;
+
+int num_PD_entry = 0;
 
 
 
@@ -97,13 +100,12 @@ void get_command(FILE *);
 void validate_pf_number(u32);
 void validate_addr(char *);
 void parse_addr(u32);
-void dump_command(void);
-void dump_vmm(void);
 int is_PT_present(u16);
 int is_UP_present(u16 pd_index, u16 pt_index);
 void create_page_table(u16 pd_index, const char* rw);
 void create_user_page(u16 pd_index, u16 pt_index,const char* rw);
 void evict_page();
+void print_outputs();
 
 
 
@@ -111,7 +113,9 @@ int main(int argc, char *argv[]){
 	
 	FILE *fp;								// stimulus file handle
 	char filename[50] = "stim.txt";			// stimulus file name, can be overwritten on cmdline
-	
+	 // if filename given on commandline use it otherwise use default filename
+	if (argc == 2)	{strcpy(filename, argv[1]);}
+	else			{strcpy(filename, "stim.txt");}
 	
 	int opt = 0;
 	
@@ -146,31 +150,29 @@ int main(int argc, char *argv[]){
 	init_PD();
 	
 	// loop through input file an feed commands to sim
+	//int count=1;
 	do {
+		//printf("getting command #%d \n",count++);
 		get_command(fp);
-		
-		if (t_flag == 1) {
-			dump_command();
-		}
-		
-		if (v_flag == 1) {
+		if (strcmp(cmd, "-v") == MATCH) {
 			printf("VMM Simulator Ver 0.1\n"); 
 			exit(-1);
 		}
 		else if (strcmp(cmd, "w") == MATCH || strcmp(cmd, "r") == MATCH ) {
 			parse_addr(addr);
-			//printf("working address\n");
-			//printf("\tPD %d\n", working_addr.PD_index );
-			//printf("\tPT %d\n", working_addr.PT_index );
-			//printf("\tOS %d\n", working_addr.UP_offset);
+			printf("working address\n");
+			printf("\tPD %d\n", working_addr.PD_index );
+			printf("\tPT %d\n", working_addr.PT_index );
+			printf("\tOS %d\n", working_addr.UP_offset);
 			
 			if (is_PT_present(working_addr.PD_index) == 1) {
 			    //check the user page present
 				if(is_UP_present(working_addr.PD_index, working_addr.PT_index) == 1)
 				{
-					//Add cycle count
+					total_cycles += 20;               //if the address exist, it considers memory access
+					printf("get here few time\n");
 				}
-				else  //the PT exist but not the user page
+				else  //the PT exists but not the user page
 				{
 				   if (available_pf == used_pf)       // no pf avail then evict one page
 						evict_page();
@@ -186,12 +188,12 @@ int main(int argc, char *argv[]){
 			    create_user_page(working_addr.PD_index, working_addr.PT_index, cmd);
 			}   
 		}
-		
-		if (d_flag == 1 && ( !strcmp(cmd,"r") || !strcmp(cmd,"w") )) {	dump_vmm(); }
+			
 		
 	} while (strcmp (cmd, "EOF"));
-	//printf("Caught EOF, we are done here. G'night\n");
-		
+	
+	num_physical_frames = 1 + num_PT + num_UP;
+	print_outputs();
 	
 }
 
@@ -251,17 +253,13 @@ void get_command(FILE *fp){
 	static int rc;
 	
 	rc = fscanf(fp, "%s", token);
-	
-	//printf("in get command and t_flag = %d\n",t_flag);
 	//printf("just after fscanf, token = %8s \teof_valid = %d\n", token, eof_valid);
-	
-	
 	if (rc == EOF && !eof_valid) {
 		fprintf(stderr, "invalid EOF in input file; this is fatal\n");
 		exit (-1);	//PWL SHOULD DO CLEANUP BEFORE EXIT!!!!!!!!!!!!!!!!
 	}
 	else if (rc == EOF ) {
-		//printf("rc = EOF\n");
+		printf("rc = EOF\n");
 		cmd = "EOF";
 	}
 	else if (first_time && strcmp(token, "-p")== MATCH) {  			//strcmp returns 0 if match
@@ -295,18 +293,9 @@ void get_command(FILE *fp){
 		need_addr= 1;
 		get_command(fp);
 	}
-	else if (strcmp(token, "-v") == MATCH) {
-		cmd = "-v";
-		v_flag = 1;
-	}
-	else if (strcmp(token, "-t") == MATCH) {
-		cmd = "-t";
-		t_flag = 1;
-	}
-	else if (strcmp(token, "-d") == MATCH) {
-		cmd = "-d";
-		d_flag = 1;
-	}
+	else if (strcmp(token, "-v") == MATCH) {v_flag = 1;}
+	else if (strcmp(token, "-t") == MATCH) {t_flag = 1;}
+	else if (strcmp(token, "-d") == MATCH) {d_flag = 1;}
 	else {
 		fprintf(stderr, "Invalid instruction sequence: got %s\n", token);
 		exit(-1);
@@ -327,7 +316,7 @@ void validate_pf_number(u32 num_pf){
 }
 
 void validate_addr(char * token) {
-	//printf("validating address\n");
+	printf("validating address\n");
 	if ( (strncmp(token, "0x", 2) == MATCH) &&
 		 (strlen(token) <= ADDR_STRING_LEN) ) {
 			 
@@ -340,10 +329,20 @@ void validate_addr(char * token) {
 	}
 }
 
-/*	takes the supplied address for the inputfile
-	and parses it into the working_addr data
-	structure
-	*/
+
+
+/* 
+*   Function: is_PT_present
+*
+*	Description: takes the supplied address for the inputfile
+*   and parses it into the working_addr data
+*   structure
+*
+*	Inputs:      32-bit addr from stimulus 
+*
+*   Outputs:     None
+*
+*/
 	
 void parse_addr(u32 addr) {
 	working_addr.PD_index  = (addr & PD_MASK) >> (PT_BITS + OS_BITS);
@@ -392,7 +391,7 @@ int is_UP_present(u16 pd_index, u16 pt_index)
 	page = (PTBR+pd_index)->address;
 	
 	//return present bit at PT entry
-	result = ((PF_t*)((size_t)page + pt_index))->present; 
+	result = ((PF_t*)((size_t)page + pt_index*sizeof(PF_t)))->present; 
 	return result;
 }
 
@@ -415,6 +414,11 @@ int is_UP_present(u16 pd_index, u16 pt_index)
 void create_page_table(u16 pd_index, const char *rw)
 {
  	PF_t* page;
+	
+	//track number of PT and max PT
+	num_PT ++;
+	if (num_PT > max_num_PT)  max_num_PT = num_PT;
+	
     //allocate memory for new PT
 	page =  (PF_t*) malloc(sizeof(PF_t)*1024);
 	
@@ -424,11 +428,13 @@ void create_page_table(u16 pd_index, const char *rw)
 	(PTBR + pd_index)->address = ((size_t)page);
 	(PTBR + pd_index)->present = 1;
 	
-	if (strcmp (rw, "w") == MATCH)
+	if (strcmp (rw, "w") == MATCH){
 	   (PTBR + pd_index)->dirty = 1;
-   
+	   printf(" get to dirty bit Pd\n");
+    }
+	num_PD_entry++;
     printf ("New Page Table added to PD as entry %d\n", pd_index);   
-    //CALL swapin;
+
 }
 
 /* 
@@ -451,17 +457,24 @@ void create_user_page(u16 pd_index, u16 pt_index, const char *rw)
 {
     size_t page;
 	
+	//track number of UP and max of UP
+	num_UP++;                           
+	if (num_UP > max_num_UP) max_num_UP = num_UP;
+	
+	used_pf ++;     //tracking number of used page frame
+	
 	page = (PTBR + pd_index)->address;  //retrieve the pointer to PT
 	
-	((PF_t*) (page + pt_index))->present = 1 ;
+	((PF_t*) (page + pt_index*8))->present = 1 ;
 	
 	if (strcmp (rw, "w")== MATCH){
-		((PF_t*) (page + pt_index))->dirty = 1;
+		((PF_t*) (page + pt_index*8))->dirty = 1;
+		 printf(" get to dirty bit IN pt \n");
 	}	
-    used_pf ++;     //tracking number of used pf
-	
-	printf("Add user page at the entry %d of PT at %lx\n", pt_index, page);
-	//CALL swap in
+    
+	//this is swap-in operation
+	total_cycles += 50000;
+	num_swap_in ++;
 }
 
 /* 
@@ -483,14 +496,18 @@ void evict_page()
 	int evict_PD_entry;    //indicate which entry in PD is evicted
 	int evict_PT_entry;    //indicate which entry in PT is evicted
 	size_t page_addr;
-	PF_t * tmp;
+	PF_t * tmp, tmp_UP;
 	
 	
 	int i,d_index, c_index;
+	
+	START:
 	num_dirty_pages = 0;
 	num_clean_pages = 0;
 	d_index = 0;
 	c_index = 0;
+	evict_PD_entry = 0;
+	evict_PT_entry = 0;
 	
 	
 	printf ("evict page \n");
@@ -518,7 +535,7 @@ void evict_page()
 	
 	//select the evict pages randomly with no write back priority
 	//evict number is an entry in PD
-	srand(1);
+	//srand(1);
 	if (num_clean_pages > 0) 
 	{
 		evict_PD_entry = evict_clean_pages[rand() % num_clean_pages];
@@ -527,14 +544,9 @@ void evict_page()
 		evict_PD_entry = evict_dirty_pages[rand() % num_dirty_pages];
 	}
 	
-	printf("number of clean page %d\n", num_clean_pages);
-	printf("number of dirty page %d\n", num_dirty_pages);
-	printf("the PT entry to evict %d\n",evict_PD_entry);
-	printf("number of frame used before evict %d\n", used_pf);
-	
-	
     //retrieve pointer to PT
 	page_addr =  ((PTBR+evict_PD_entry)->address);
+	
 	d_index = 0;
 	c_index = 0;
 	num_clean_pages = 0;
@@ -542,74 +554,89 @@ void evict_page()
 	 
 	//loop thr PT to search for UP to evict
 	for (i = 0; i < PF_SIZE ; i++)
-	{
-	    if ( ((PF_t*)(page_addr+i))->present == 1)
-		{		
-		   evict_clean_pages[c_index] = i;
-		   c_index++;
-		   num_clean_pages++;
-		}
-		else 
-		{
-		    evict_clean_pages[c_index] = i;
-			c_index ++;
-            num_clean_pages ++;
-        }				
+	{  
+	    if ( ((PF_t*)(page_addr+i*8))->present == 1)
+		{	
+		    if (((PF_t*)(page_addr+i*8))->dirty == 1)
+		    {
+			   evict_dirty_pages[d_index] = i;
+			   d_index++;
+		       num_dirty_pages++;
+			}
+			else 
+			{
+				evict_clean_pages[c_index] = i;
+				c_index ++;
+				num_clean_pages ++;
+			}				
+		}	
 	}  //end PT for loop search
-		   
+		
+	printf("number dirty page %d \n", num_dirty_pages);
+	printf("number clean page %d \n", num_clean_pages);
+	
 	if (num_clean_pages > 0)                         //evict clean page
 	{
 		evict_PT_entry = evict_clean_pages[rand() % num_clean_pages];
-		((PF_t*)page_addr + evict_PT_entry)-> present = 0;    //clear the present bit
-		used_pf --;                                  //set number user PF
+		((PF_t*)(page_addr + evict_PT_entry))-> present = 0;    //clear the present bit in PT
+		printf("move the clean page \n");
+		used_pf --;       		                                //track number of frames
+		num_UP--;
+		num_pure_replace++;
 	}
 	else if (num_dirty_pages >0)                     //Evict dirty page
 	{
 		evict_PT_entry = evict_dirty_pages[rand() % num_dirty_pages];
-		((PF_t*)page_addr + evict_PT_entry)-> present = 0;
-		((PF_t*)page_addr + evict_PT_entry)-> dirty   = 0;
-		used_pf --;
-		//CALL swap-out ; 
+		((PF_t*)(page_addr + evict_PT_entry))-> present = 0;
+		((PF_t*)(page_addr + evict_PT_entry))-> dirty   = 0;
+		printf("move the dirty page \n");
+		used_pf --;                                  //track number of frames
+		total_cycles +=50000;                        //swap-out cycles
+		num_UP--;
+		num_swap_out ++;
 	} 
-	else {  //evic its own PT	
+	else if (num_UP == 0)                            //only evict PT when there is no longer 
+	                                                 //have any user page left
+	{  //evic its own PT	
 		//clear the entry in PD
 		((PF_t*) (PD + evict_PD_entry))->present  = 0;
 		((PF_t*) (PD + evict_PD_entry))->dirty    = 0;
-		used_pf --;
+		printf ("move the pt\n");
+		num_PD_entry--;                               //track number entry in PD
+		used_pf --;                                   //track number of frames 
+		num_PT --;                                    //PT removed
 	}
-
-	
+	else  //keep looping the until all the user pages removed
+		goto START;
 }
 
-void dump_command(){
-	if ( !strcmp(cmd, "r") || !strcmp(cmd, "w") ) {
-		printf("current command: %s %08X\n", cmd, addr);
-	}
-}
+/* 
+*   Function: print_outputs
+*
+*	Description: This function is to print out the outputs
+*
+*	Inputs:      None
+*
+*   Outputs:     None
+*
+*/
+void print_outputs()
+{
+	printf( " Number of PD Entries     : %d \n", num_PD_entry);
+	printf( "\n");
+	printf( " Number of Page Tables    : %d \n", num_PT);
+	printf( " Max Number of Page Tables: %d \n", max_num_PT);
+	printf( "\n");
+	printf( " Number of User Pages     : %d \n", num_UP);
+	printf( " Max Number of User Pages : %d \n", max_num_UP);
+	printf( "\n");
+	printf( " Number of Physical Frames: %d \n", num_physical_frames);
+	printf( "\n");
+	printf( " Number of swap-in        : %d \n", num_swap_in);
+	printf( " Number of swap-out       : %d \n", num_swap_out);
+	printf( " Number of Pure Replacement: %d\n", num_pure_replace);
+	printf( "\n");
+	printf( " Number of Execution Cycles: %d\n", total_cycles);
 
-void dump_vmm() {
-	int i,j;
-	int PT_base_addr;
-	
-	for (i=0; i < PF_SIZE; i++) {
-		if (PD[i].present != 0) {
-			PT_base_addr = PD[i].address;
-			printf("===============================================\n");
-			printf("PD index = %04d ", i);
-			printf("addr = 0x%08X ", PD[i].address);
-			printf("p = %d ",PD[i].present);
-			printf("d = %d\n",PD[i].dirty);
-			printf("================================================\n");
-			for (j=0; j < PF_SIZE; j++) {
-				if ( (*(PF_t*)(PT_base_addr)).present != 0 ) {
-					printf("\t\tPT_index = %d ",j);
-					printf("addr = 0x%08X ", (*(PF_t*)(PT_base_addr)).address);
-					printf("p = %d ",        (*(PF_t*)(PT_base_addr)).present);
-					printf("d = %d\n",       (*(PF_t*)(PT_base_addr)).dirty);
-				}
-				PT_base_addr ++;
-			}
-		}
-	}
-	printf("\n\n\n\n\n");
+
 }
